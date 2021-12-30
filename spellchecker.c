1941c00405bdbc7333a984f8a303da20de88ff37
Matthew Spence <mspence5555@gmail.com>
2021-12-29 21:29:26 -0600

    finished loading dictionary into memory

--- spellchecker.c
+++ spellchecker.c
@@ -6,24 +6,40 @@
 #include <strings.h>
 #include <ctype.h>
 
-const int max_word_length = 18;
+const int max_word_length = 28;
 typedef struct node {
-	char word[18];
+	char word[28];
 	struct node *next;
 }
 node;
 // prototypes
 void store_hash(char current_word[max_word_length], node *head, int position);
 
-node *buckets[25];
+node *buckets[26];
+int buckets_length = sizeof(buckets) / sizeof(*buckets);
 
-int main(void) {
+int main(int argc, char *argv[]) {
 	
-	FILE *dict = fopen("dictionary.txt", "r");
+	if(argc != 3) {
+		printf("usage: ./spellchecker dictionary.txt checkspelling.txt\n");
+		return 1;
+	}
+	FILE *dict = fopen(argv[1], "r");
+	FILE *check_spelling = fopen(argv[2], "r");
+	if(dict == NULL) {
+		printf("could not open file %s\n", argv[1]);
+		return 1;
+	}
+	else if(check_spelling == NULL) {
+		printf("could not open file %s\n", argv[2]);
+		return 1;
+	}
+	
+	
 	char current_word[max_word_length];
 	
 	// allocate memory for each node in buckets array and insert head node
-	for(int i = 0; i < 25; i++) {
+	for(int i = 0; i < buckets_length; i++) {
 		node *head = malloc(sizeof(node));
 		if(head == NULL) {
 			printf("failed, out of memory\n");
@@ -33,24 +49,17 @@ int main(void) {
 		buckets[i] = head;
 	}
 
+	// store each word from dictionary into buckets[] as a linked list
 	int position;
 	while(fscanf(dict, "%s", current_word) != EOF) {
+		// determine index which current_word should be stored in buckets[]
 		position = ((int)tolower(current_word[0]) - 97);
 		store_hash(current_word, buckets[position], position);
 	}
 	
-	
-	
-	
 
-	printf("%s\n", buckets[12]->word);
-	// printf("%s\n", buckets[0]->next->word);
-	// printf("%s\n", buckets[0]->next->next->word);
-	
-
-	
 	// free memory allocted to buckets array (which contains nodes)
-	for(int i = 0; i < 25; i++) {
+	for(int i = 0; i < buckets_length; i++) {
 		while(buckets[i] != NULL) {
 			node *temp = buckets[i]->next;
 			free(buckets[i]);
@@ -102,3 +111,15 @@ void store_hash(char current_word[], node *head, int position) {
 	// 	alpha[j] = i;
 	// 	j++;
 	// }
+
+// print all words from linked lists
+// int word_count = 0;
+// 	for(int i = 0; i < buckets_length; i++) {
+// 		for(node *tmp = buckets[i]; tmp != NULL; tmp = tmp->next) {
+		
+// 			printf("%s\n", tmp->word);
+// 			word_count++;
+		
+// 		}
+// 	}
+// 	printf("word cound: %i", word_count - buckets_length);
