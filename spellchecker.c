effab969d8fae45a064bd60d57feb013c1f96607
Matthew Spence <mspence5555@gmail.com>
2021-12-30 09:16:30 -0600

    working in free() all allocated memory

--- spellchecker.c
+++ spellchecker.c
@@ -1,9 +1,10 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h>
+//#include <stdint.h>
 #include <string.h>
-#include <strings.h>
+//#include <strings.h>
+//#include <strings.h>
 #include <ctype.h>
 
 const int max_word_length = 28;
@@ -15,6 +16,7 @@ node;
 // prototypes
 void store_hash(char current_word[max_word_length], node *head, int position, bool is_check);
 void compare_lists(void);
+void free_memory(void);
 
 node *buckets[26];
 node *check_buckets[26];
@@ -45,6 +47,7 @@ int main(int argc, char *argv[]) {
 		node *head = malloc(sizeof(node));
 		if(head == NULL) {
 			printf("failed, out of memory\n");
+			free_memory();
 			return 1;
 		}
 		head->next = NULL;
@@ -69,7 +72,61 @@ int main(int argc, char *argv[]) {
 
 	// call funtion that will compare lists
 	compare_lists();
+	free_memory();
+	fclose(dict);
+	fclose(check_spelling);
+}
+
+void store_hash(char current_word[], node *head, int position, bool is_check) {
 	
+	if(is_check == false) {
+		// create new node and give value of current_word
+		node *curr_word = malloc(sizeof(node));
+		if(curr_word == NULL) {
+			printf("failed, out of memory\n");
+			free_memory();
+			exit(1);
+		}
+		strcpy(curr_word->word, current_word);
+		curr_word->next = head; // point new node to current head
+		buckets[position] = curr_word; // assign index 0 to new node
+
+		return;
+	}
+	else {
+		// create new node and give value of current_word
+		node *curr_word = malloc(sizeof(node));
+		if(curr_word == NULL) {
+			printf("failed, out of memory\n");
+			free_memory();
+			exit(1);
+		}
+		strcpy(curr_word->word, current_word);
+		curr_word->next = head; // point new node to current head
+		check_buckets[position] = curr_word; // assign index 0 to new node
+
+		return;
+	}
+}
+
+void compare_lists(void) {
+	// todo:
+	// 1. check each word in check_buckets[i] against every word in buckets[i]
+	// loop each word in check_buckets[i] * buckets[i].length
+
+	// print all words from linked lists
+	// int word_count = 0;
+	// for(int i = 0; i < buckets_length; i++) {
+	// 	for(node *tmp = check_buckets[i]; tmp != NULL; tmp = tmp->next) {
+	// 		printf("%s\n", tmp->word);
+	// 		word_count++;
+	// 	}
+	// }
+	// printf("word cound: %i", word_count - buckets_length);
+}
+
+
+void free_memory(void) {
 
 	// free memory allocted to buckets array (which contains nodes)
 	for(int i = 0; i < buckets_length; i++) {
@@ -86,54 +143,7 @@ int main(int argc, char *argv[]) {
 			check_buckets[i] = temp;
 		}
 	}
-	fclose(dict);
-	fclose(check_spelling);
-}
-
-void store_hash(char current_word[], node *head, int position, bool is_check) {
-	
-	if(is_check == false) {
-		// create new node and give value of current_word
-		node *curr_word = malloc(sizeof(node));
-		if(curr_word == NULL) {
-			printf("failed, out of memory\n");
-			return;
-		}
-		strcpy(curr_word->word, current_word);
-		curr_word->next = head; // point new node to current head
-		buckets[position] = curr_word; // assign index 0 to new node
-
-		return;
-	}
-	else {
-		// create new node and give value of current_word
-		node *curr_word = malloc(sizeof(node));
-		if(curr_word == NULL) {
-			printf("failed, out of memory\n");
-			return;
-		}
-		strcpy(curr_word->word, current_word);
-		curr_word->next = head; // point new node to current head
-		check_buckets[position] = curr_word; // assign index 0 to new node
-
-		return;
-	}
-}
-
-void compare_lists(void) {
-	// todo:
-	// 1. check each word in check_buckets[i] against every word in buckets[i]
-	// loop each word in check_buckets[i] * buckets[i].length
-
-	// print all words from linked lists
-	int word_count = 0;
-	for(int i = 0; i < buckets_length; i++) {
-		for(node *tmp = check_buckets[i]; tmp != NULL; tmp = tmp->next) {
-			printf("%s\n", tmp->word);
-			word_count++;
-		}
-	}
-	printf("word cound: %i", word_count - buckets_length);
+	return;
 }
 
 
